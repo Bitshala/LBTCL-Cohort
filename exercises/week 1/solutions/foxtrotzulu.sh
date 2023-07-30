13bef94758f73e4a3b9c3034990a9a0124bf2a8a
FoxtrotZulu <111104979+Foxtrot-Zulu@users.noreply.github.com>
2023-07-30 03:38:00 -0300

    Update foxtrotzulu.sh
    make corrections as suggested

--- exercises/week 1/solutions/foxtrotzulu.sh
+++ exercises/week 1/solutions/foxtrotzulu.sh
@@ -12,24 +12,32 @@ wget https://bitcoincore.org/bin/bitcoin-core-25.0/bitcoin-25.0-x86_64-linux-gnu
 wget https://bitcoincore.org/bin/bitcoin-core-25.0/SHA256SUMS
 wget https://bitcoincore.org/bin/bitcoin-core-25.0/SHA256SUMS.asc
 
-
 # Add Core Devs public keys
 git clone https://github.com/bitcoin-core/guix.sigs
 gpg --import guix.sigs/builder-keys/*
 
-# Verify the signature
-sha256sum --ignore-missing -c SHA256SUMS | grep -i 'OK'
-gpg --verify SHA256SUMS.asc | grep 'gpg: Good signature'
+# Verify hash
+CHECKED=$(sha256sum --ignore-missing --check SHA256SUMS)
+
+if [[ ( $CHECKED  == "bitcoin-25.0-x86_64-linux-gnu.tar.gz: OK" ) ]]; then
+        echo "File is verified OK"
+else
+        echo "File didn't pass the verification"
+        exit 1
+fi
+
+# Verify signatures
+gpg --verify SHA256SUMS.asc
 
 # Print a message to terminal
 echo "Binary signature verification successful"
 
-# Copy the binaries to /usr/local/bin/
-sudo cp bitcoin-25.0-x86_64-linux-gnu.tar.gz /usr/local/bin/
-sudo tar -xzf bitcoin-25.0-x86_64-linux-gnu.tar.gz -C /usr/local/bin/
+# Extract and copy the binaries to /usr/local/bin/
+tar -xzf bitcoin-25.0-x86_64-linux-gnu.tar.gz
 
-# Add dir to path
-echo "export PATH=/usr/local/bin/bitcoin-25.0/bin:$PATH" >> ~/.bashrc && source ~/.bashrc
+sudo cp -R bitcoin-25.0/bin /usr/local/bin
+
+echo "Instalation successful"
 
 # Delete downloaded files
 rm bitcoin-25.0-x86_64-linux-gnu.tar.gz && rm SHA256SUMS && rm SHA256SUMS.asc
@@ -41,56 +49,56 @@ mkdir ~/.bitcoin
 echo "regtest=1" >> ~/.bitcoin/bitcoin.conf && echo "fallbackfee=0.0001" >> ~/.bitcoin/bitcoin.conf && echo "server=1" >> ~/.bitcoin/bitcoin.conf && echo "txindex=1" >> ~/.bitcoin/bitcoin.conf
 
 # Start bitcoind
-bitcoind -regtest -daemon
+bitcoind -daemon
 sleep 5
 
 #Create the Miner and Trader wallets
-bitcoin-cli -regtest createwallet Miner
-bitcoin-cli -regtest createwallet Trader
+bitcoin-cli createwallet Miner
+bitcoin-cli createwallet Trader
 
 #Generate address from the Miner wallet
-bitcoin-cli -regtest loadwallet Miner
-ADDRESS=$(bitcoin-cli -regtest -rpcwallet=Miner getnewaddress "Mining Reward")
+bitcoin-cli loadwallet Miner
+ADDRESS=$(bitcoin-cli -rpcwallet=Miner getnewaddress "Mining Reward")
 
 #Mine new blocks to the address until the wallet balance is positive
-bitcoin-cli -regtest generatetoaddress 101 ${ADDRESS}
+bitcoin-cli generatetoaddress 101 ${ADDRESS}
 
 # Print a comment describing why wallet balance for block rewards behaves that way
 echo "Wallet balance for block rewards starts at zero because block rewards are not immediately spendable. They are spendable after 100 blocks from mined block."
 
 # Print the balance of the Miner wallet
-BALANCE=$(bitcoin-cli -regtest -rpcwallet=Miner getbalances)
-echo "The balance of the Miner wallet is $BALANCE."
+BALANCE=$(bitcoin-cli -rpcwallet=Miner getbalances)
+echo "The balance of the Miner wallet is $BALANCE"
 
 #USAGE
 
 # Create a receiving address labeled "Received" from Trader wallet
-RECEIVE_ADDRESS=$(bitcoin-cli -regtest -rpcwallet=Trader getnewaddress "Received")
+RECEIVE_ADDRESS=$(bitcoin-cli -rpcwallet=Trader getnewaddress "Received")
 
 # Send a transaction paying 20 BTC from Miner wallet to Trader's wallet
-TRANSACTION_ID=$(bitcoin-cli -regtest -rpcwallet=Miner sendtoaddress $RECEIVE_ADDRESS 20)
+TX_ID=$(bitcoin-cli -rpcwallet=Miner sendtoaddress $RECEIVE_ADDRESS 20)
 
 # Fetch the unconfirmed transaction from the node's mempool and print the result
-UNCONFIRMED_TRANSACTION=$(bitcoin-cli -regtest -rpcwallet=Miner getmempoolentry $TRANSACTION_ID)
+UNCONFIRMED_TRANSACTION=$(bitcoin-cli -rpcwallet=Miner getmempoolentry $TX_ID)
 
 # Confirm the transaction by creating 1 more block
-bitcoin-cli -regtest -rpcwallet=Miner generatetoaddress 1 $ADDRESS
+bitcoin-cli -rpcwallet=Miner generatetoaddress 1 $ADDRESS
 
 # Fetch the following details of the transaction and print them into terminal
-TXID=$(echo $UNCONFIRMED_TRANSACTION | jq -r '.txid')
-FROM=$(echo $UNCONFIRMED_TRANSACTION | jq -r '.inputs[0].prevout.address')
-AMOUNT=$(echo $UNCONFIRMED_TRANSACTION | jq -r '.inputs[0].amount')
-SEND=$(echo $UNCONFIRMED_TRANSACTION | jq -r '.outputs[0].address')
-SENT=$(echo $UNCONFIRMED_TRANSACTION | jq -r '.outputs[0].amount')
-CHANGE=$(echo $UNCONFIRMED_TRANSACTION | jq -r '.outputs[1].address')
-CHANGE_BACK=$(echo $UNCONFIRMED_TRANSACTION | jq -r '.outputs[1].amount')
-FEES=$(echo $UNCONFIRMED_TRANSACTION | jq -r '.fee')
-BLOCK=$(bitcoin-cli -regtest getblockcount)
-MINER_BALANCE=$(bitcoin-cli -regtest -rpcwallet=Miner getbalances)
-TRADER_BALANCE=$(bitcoin-cli -regtest -rpcwallet=Trader getbalances)
+TXID=$(echo $TX_ID | jq -r '.txid')
+FROM=$(echo $TX_ID | jq -r '.inputs[0].prevout.address')
+AMOUNT=$(echo $TX_ID | jq -r '.inputs[0].amount')
+SEND=$(echo $TX_ID | jq -r '.outputs[0].address')
+SENT=$(echo $TX_ID | jq -r '.outputs[0].amount')
+CHANGE=$(echo $TX_ID | jq -r '.outputs[1].address')
+CHANGE_BACK=$(echo $TX_ID | jq -r '.outputs[1].amount')
+FEES=$(echo $TX_ID | jq -r '.fee')
+BLOCK=$(bitcoin-cli getblockcount)
+MINER_BALANCE=$(bitcoin-cli -rpcwallet=Miner getbalances)
+TRADER_BALANCE=$(bitcoin-cli -rpcwallet=Trader getbalances)
 
 echo "Transaction details:"
-echo "TXID: $TXID"
+echo "TXID: $TX_ID"
 echo "From, Amount: $FROM, $AMOUNT"
 echo "Send, Amount: $SEND, $SENT"
 echo "Change, Amount: $CHANGE, $CHANGE_BACK"
