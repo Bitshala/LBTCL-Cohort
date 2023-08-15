f7c55d10e5ccaaff6480fbb461a9eda9f9826aa7
emjshrx <emjshrx@gmail.com>
2023-08-15 17:24:25 +0530

    descriptor multi sig

--- exercises/week 3/solutions/emjshrx.sh
+++ exercises/week 3/solutions/emjshrx.sh
@@ -18,7 +18,7 @@ echo "Creating wallets .... "
 bitcoin-cli -datadir=/tmp/emjshrx createwallet "Miner" >/dev/null
 bitcoin-cli -datadir=/tmp/emjshrx createwallet "Alice" >/dev/null
 bitcoin-cli -datadir=/tmp/emjshrx createwallet "Bob"  >/dev/null
-mineraddr=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Miner getnewaddress "Mining Reward" legacy)
+mineraddr=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Miner getnewaddress "Mining Reward")
 aliceaddr=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Alice getnewaddress "Alice")
 bobaddr=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Bob getnewaddress "Bob")
 
@@ -32,10 +32,29 @@ echo "Alice balance : $(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Alice getba
 echo "Bob balance : $(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Bob getbalance)"
 
 echo "Creating MultiSig address ...."
-alicehex=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Alice getaddressinfo $aliceaddr | jq ".pubkey")
-bobhex=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Bob getaddressinfo $bobaddr | jq ".pubkey")
-multijson=$(bitcoin-cli -rpcwallet=Alice -datadir=/tmp/emjshrx createmultisig 2 "[$alicehex,$bobhex]")
-multiaddr=$(echo $multijson | jq ".address")
+xpub_internal_alice=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Alice listdescriptors | jq '.descriptors | [.[] | select(.desc | startswith("wpkh") and contains("/1/*"))][0] | .desc' | grep -Po '(?<=\().*(?=\))')
+xpub_external_alice=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Alice listdescriptors | jq '.descriptors | [.[] | select(.desc | startswith("wpkh") and contains("/0/*") )][0] | .desc' | grep -Po '(?<=\().*(?=\))')
+
+xpub_internal_bob=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Bob listdescriptors | jq '.descriptors | [.[] | select(.desc | startswith("wpkh") and contains("/1/*"))][0] | .desc' | grep -Po '(?<=\().*(?=\))')
+xpub_external_bob=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Bob listdescriptors | jq '.descriptors | [.[] | select(.desc | startswith("wpkh") and contains("/0/*") )][0] | .desc' | grep -Po '(?<=\().*(?=\))')
+
+external_desc="wsh(sortedmulti(2,$xpub_external_alice,$xpub_external_bob))"
+internal_desc="wsh(sortedmulti(2,$xpub_internal_alice,$xpub_internal_bob))"
+
+external_desc_sum=$(bitcoin-cli -datadir=/tmp/emjshrx getdescriptorinfo $external_desc | jq '.descriptor')
+internal_desc_sum=$(bitcoin-cli -datadir=/tmp/emjshrx getdescriptorinfo $internal_desc | jq '.descriptor')
+
+multisig_ext_desc="{\"desc\": $external_desc_sum, \"active\": true, \"internal\": false, \"timestamp\": \"now\"}"
+multisig_int_desc="{\"desc\": $internal_desc_sum, \"active\": true, \"internal\": true, \"timestamp\": \"now\"}"
+
+multisig_desc="[$multisig_ext_desc, $multisig_int_desc]"
+
+bitcoin-cli -datadir=/tmp/emjshrx -named createwallet wallet_name="multisig_wallet" disable_private_keys=true blank=true
+
+bitcoin-cli  -datadir=/tmp/emjshrx -rpcwallet="multisig_wallet" importdescriptors "$multisig_desc"
+
+bitcoin-cli  -datadir=/tmp/emjshrx -rpcwallet="multisig_wallet" getwalletinfo
+multiaddr=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet="multisig_wallet" getnewaddress "Multisig")
 echo "The multisig address : $multiaddr"
 
 read -n 1 -s -r -p "Press any key to continue"
@@ -45,7 +64,7 @@ input_0=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Alice listunspent | jq ".
 input_1=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Bob listunspent | jq ".[0].txid")
 vout_0=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Alice listunspent | jq ".[0].vout")
 vout_1=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Bob listunspent | jq ".[0].vout")
-fund_multi_psbt=$(bitcoin-cli -datadir=/tmp/emjshrx createpsbt '[{"txid":'$input_0',"vout":'$vout_0'},{"txid":'$input_1',"vout":'$vout_1'}]' '[{'$multiaddr':'20'},{"'$aliceaddr'":'9.999'},{"'$bobaddr'":'9.999'}]')
+fund_multi_psbt=$(bitcoin-cli -datadir=/tmp/emjshrx createpsbt '[{"txid":'$input_0',"vout":'$vout_0'},{"txid":'$input_1',"vout":'$vout_1'}]' '[{"'$multiaddr'":'20'},{"'$aliceaddr'":'9.999'},{"'$bobaddr'":'9.999'}]')
 alice_fund_multi_psbt=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Alice walletprocesspsbt $fund_multi_psbt | jq ".psbt" | tr -d '"')
 alice_bob_fund_multi_psbt=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Bob walletprocesspsbt $alice_fund_multi_psbt | jq ".psbt" | tr -d '"')
 bitcoin-cli -datadir=/tmp/emjshrx decodepsbt $alice_bob_fund_multi_psbt
@@ -57,16 +76,18 @@ bitcoin-cli -datadir=/tmp/emjshrx generatetoaddress 1 $mineraddr >/dev/null
 read -n 1 -s -r -p "Press any key to continue"
 clear
 
-#echo "Breaking MultiSig address ...."
-#break_multi_psbt=$(bitcoin-cli -datadir=/tmp/emjshrx createpsbt '[{"txid":"'$fund_multi_txid'","vout":0}]' '[{"'$aliceaddr'":'9.999'},{"'$bobaddr'":'9.999'}]')
-#alice_break_multi_psbt=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Alice walletprocesspsbt $break_multi_psbt | jq ".psbt" | tr -d '"')
-#alice_bob_break_multi_psbt=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Bob walletprocesspsbt $alice_break_multi_psbt | jq ".psbt" | tr -d '"')
-#final_break_multi_hex=$(bitcoin-cli -datadir=/tmp/emjshrx finalizepsbt $alice_bob_break_multi_psbt | jq ".hex" | tr -d '"')
-#break_multi_txid=$(bitcoin-cli -datadir=/tmp/emjshrx sendrawtransaction $final_break_multi_hex)
-#echo "Multi sig Breaking Tx broadcasted with txid: $break_multi_txid"
+echo "Breaking MultiSig address ...."
+break_multi_psbt=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet="multisig_wallet" -named walletcreatefundedpsbt outputs='[{"'$aliceaddr'":'9.999'},{"'$bobaddr'":'9.999'}]' | jq ".psbt" | tr -d '"')
+alice_break_multi_psbt=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Alice walletprocesspsbt $break_multi_psbt | jq ".psbt" | tr -d '"')
+alice_bob_break_multi_psbt=$(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Bob walletprocesspsbt $alice_break_multi_psbt | jq ".psbt" | tr -d '"')
+final_break_multi_hex=$(bitcoin-cli -datadir=/tmp/emjshrx finalizepsbt $alice_bob_break_multi_psbt | jq ".hex" | tr -d '"')
+break_multi_txid=$(bitcoin-cli -datadir=/tmp/emjshrx sendrawtransaction $final_break_multi_hex)
+echo "Multi sig Breaking Tx broadcasted with txid: $break_multi_txid"
+bitcoin-cli -datadir=/tmp/emjshrx generatetoaddress 1 $mineraddr >/dev/null
 
-bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Alice getbalance
-bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Bob getbalance
+echo "Alice balance : $(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Alice getbalance)"
+echo "Bob balance : $(bitcoin-cli -datadir=/tmp/emjshrx -rpcwallet=Bob getbalance)"
+echo "They have same balances. Not sure how they would be different"
 
 read -n 1 -s -r -p "Press any key to continue"
 clear
