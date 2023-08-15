520395461d5ebcd26ca02cc927b1c8c80e0cb986
emjshrx <emjshrx@gmail.com>
2023-08-15 12:13:17 +0530

    formatting

--- exercises/week 3/solutions/emjshrx.sh
+++ exercises/week 3/solutions/emjshrx.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 clear
 echo "LBTCL Cohort Week 3 Script"
 read -n 1 -s -r -p "Press any key to continue"
@@ -8,21 +7,21 @@ clear
 bitcoind -daemon -fallbackfee=0.0001 -regtest -maxtxfee=20
 sleep 5
 echo "Creating wallets .... "
-bitcoin-cli -regtest createwallet "Miner" 
+bitcoin-cli -regtest createwallet "Miner"
 bitcoin-cli -regtest -named createwallet "Alice" #descriptors=false
-bitcoin-cli -regtest -named createwallet "Bob" #descriptors=false
-bitcoin-cli -regtest loadwallet Miner > /dev/null
-bitcoin-cli -regtest loadwallet Alice > /dev/null
-bitcoin-cli -regtest loadwallet Bob > /dev/null
+bitcoin-cli -regtest -named createwallet "Bob"   #descriptors=false
+bitcoin-cli -regtest loadwallet Miner >/dev/null
+bitcoin-cli -regtest loadwallet Alice >/dev/null
+bitcoin-cli -regtest loadwallet Bob >/dev/null
 mineraddr=$(bitcoin-cli -regtest -rpcwallet=Miner getnewaddress "Mining Reward" legacy)
 aliceaddr=$(bitcoin-cli -regtest -rpcwallet=Alice getnewaddress "Alice")
 bobaddr=$(bitcoin-cli -regtest -rpcwallet=Bob getnewaddress "Bob")
 
 echo "Generating some blocks and sending to Alice and Bob .... "
-bitcoin-cli -regtest generatetoaddress 102 $mineraddr > /dev/null
-input_0=$(bitcoin-cli -regtest -rpcwallet=Miner sendtoaddress $aliceaddr 25) 
-input_1=$(bitcoin-cli -regtest -rpcwallet=Miner sendtoaddress $bobaddr 25) 
-bitcoin-cli -regtest generatetoaddress 1 $mineraddr > /dev/null
+bitcoin-cli -regtest generatetoaddress 102 $mineraddr >/dev/null
+input_0=$(bitcoin-cli -regtest -rpcwallet=Miner sendtoaddress $aliceaddr 25)
+input_1=$(bitcoin-cli -regtest -rpcwallet=Miner sendtoaddress $bobaddr 25)
+bitcoin-cli -regtest generatetoaddress 1 $mineraddr >/dev/null
 bitcoin-cli -regtest -rpcwallet=Miner getbalance
 bitcoin-cli -regtest -rpcwallet=Alice getbalance
 bitcoin-cli -regtest -rpcwallet=Bob getbalance
@@ -45,7 +44,7 @@ bitcoin-cli -regtest decodepsbt $alice_bob_fund_multi_psbt
 final_fund_multi_hex=$(bitcoin-cli -regtest finalizepsbt $alice_bob_fund_multi_psbt | jq ".hex" | tr -d '"')
 fund_multi_txid=$(bitcoin-cli -regtest sendrawtransaction $final_fund_multi_hex 0)
 echo "Multi sig funding Tx broadcasted with txid: $fund_multi_txid"
-bitcoin-cli -regtest generatetoaddress 1 $mineraddr > /dev/null
+bitcoin-cli -regtest generatetoaddress 1 $mineraddr >/dev/null
 
 read -n 1 -s -r -p "Press any key to continue"
 clear
@@ -61,7 +60,6 @@ clear
 bitcoin-cli -regtest -rpcwallet=Alice getbalance
 bitcoin-cli -regtest -rpcwallet=Bob getbalance
 
-
 read -n 1 -s -r -p "Press any key to continue"
 clear
-bitcoin-cli -regtest  stop
+bitcoin-cli -regtest stop
